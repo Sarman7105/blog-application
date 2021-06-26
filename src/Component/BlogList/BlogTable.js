17f5ecbbc2494cba116658342a0b809facfc3343
Sarman7105 <sarman7105@gmail.com>
2021-06-26 18:34:30 +0600

    table created to show blogs

--- src/Component/BlogList/BlogTable.js
+++ src/Component/BlogList/BlogTable.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const BlogTable = ({blogs}) => {
-    return (
-        <div>
-            this is blog table
-        </div>
-    );
+const BlogTable = ({ blogs }) => {
+    // console.log(blogs);
+    let index = 0;
+	return (
+		<div>
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Title</th>
+						<th scope="col">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+                    {blogs.map((blog) => {
+                        index++;
+                        return (
+                            <tr>
+                                <th scope="row">{ index}</th>
+							<td>{blog.title}</td>
+                            <td>
+
+                                <button className="btn btn-danger">Delete </button>
+                                    
+                            </td>
+						</tr>
+                        )
+						
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default BlogTable;
